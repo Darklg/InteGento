9788a1a230fd5b40c377323eb467e487b4cf577c
Darklg <kev.rocher@gmail.com>
2014-07-26 12:53:06 +0200

    Default cart template

--- skin/frontend/integento/default/js/events.js
+++ skin/frontend/integento/default/js/events.js
@@ -23,4 +23,35 @@ jQuery(document).ready(function() {
 
     }());
 
+    /* Cart */
+
+    (function() {
+        if ($jQBody.hasClass('checkout-cart-index')) {
+            // Increment / Decrement
+            jQuery('[data-decrement], [data-increment]').on('click', function(e) {
+                e.preventDefault();
+                var $this = jQuery(this),
+                    newVal = 0,
+                    val = 0,
+                    $input = false,
+                    type = 'decrement';
+                if ($this.data('increment')) {
+                    type = 'increment';
+                }
+
+                $input = jQuery('#' + $this.data(type));
+                if ($input.length > 0) {
+                    val = parseInt($input.val(), 10);
+                    newVal = Math.max(0, val - 1);
+                    if (type == 'increment') {
+                        newVal = val + 1;
+                    }
+
+                    $input.val(newVal);
+                    $input.trigger('change');
+                }
+            });
+        }
+    }());
+
 });
