8165ebe470edc3a2ffe524ae0ccbeeed01769328
Darklg <kev.rocher@gmail.com>
2014-08-22 22:50:24 +0200

    Implement Grunt : Compass & ImageOptim

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    var skinDir = 'skin/frontend/integento/default/';
+
+    // load tasks
+    grunt.loadNpmTasks('grunt-imageoptim');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+
+    grunt.initConfig({
+        imageoptim: {
+            dist: {
+                src: [skinDir + 'images/*.png']
+            }
+        },
+        compass: {
+            dist: {
+                options: {
+                    config: 'config.rb'
+                }
+            }
+        }
+    });
+
+    // alias
+    grunt.registerTask('default', ['compass', 'imageoptim']);
+};
