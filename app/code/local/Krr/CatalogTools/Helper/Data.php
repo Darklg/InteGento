1c9ff465a49fd54b1c0e42f0ee9f7a099b4288f3
Kevin Rocher <kevin@rocher-dev.com>
2016-10-04 22:55:07 +0200

    Kr_CatalogTools v 0.1.2 : Choose template & cleaner code.

--- app/code/local/Krr/CatalogTools/Helper/Data.php
+++ app/code/local/Krr/CatalogTools/Helper/Data.php
@@ -1,14 +1,17 @@
 <?php
-class Krr_CatalogTools_Helper_Data extends Mage_Core_Helper_Abstract
-{
-    public function getProductHTML($_product, $block_type = 'catalog/product_list') {
-        $block = Mage::app()
-            ->getLayout()
+class Krr_CatalogTools_Helper_Data extends Mage_Core_Helper_Abstract {
+    /**
+     * Get HTML for Product item
+     * @param  object $_product     Magento Product item
+     * @param  string $block_type   Block type
+     * @param  string $template     Custom template
+     * @return string               HTML to display
+     */
+    public function getProductHTML($_product, $block_type = 'catalog/product_list', $template = 'krr_catalogtools/product-item.phtml') {
+        return $this->getLayout()
             ->createBlock($block_type)
-            ->setTemplate('krr_catalogtools/product-item.phtml');
-
-        $block->setProduct($_product);
-
-        return $block->toHtml();
+            ->setProduct($_product)
+            ->setTemplate($template)
+            ->toHtml();
     }
 }
